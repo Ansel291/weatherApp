48a49aee4a3fed1ac8078c0349df70e2af4aa23c
jason <jason@herman-scheer.com>
2022-07-03 13:31:06 -0500

    created context

--- src/components/currentConditions/CurrentCondition.jsx
+++ src/components/currentConditions/CurrentCondition.jsx
@@ -1,42 +1,20 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useContext } from 'react'
 import Spinner from '../layout/Spinner'
+import WeatherContext from '../../context/weather/WeatherContext'
 
 function CurrentCondition() {
-  const [currentConditions, setCurrentConditions] = useState([])
-  /*
-  const [textCondition, setTextCondition] = useState([])
-  const [imageURL, setImageURL] = useState('')
-  */
-  const [currentConditionsSecondary, setCurrentConditionsSecondary] = useState(
-    []
-  )
-  const [currentLocation, setCurrentLocation] = useState([])
-  const [loading, setLoading] = useState(true)
+  const {
+    currentConditions,
+    currentConditionsSecondary,
+    currentLocation,
+    loading,
+    fetchCurrentCondition,
+  } = useContext(WeatherContext)
 
   useEffect(() => {
     //console.log('use Effect fired')
     fetchCurrentCondition()
   }, [])
-
-  const fetchCurrentCondition = async () => {
-    const response = await fetch(
-      `http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHER_API_TOKEN}&q=los angeles&aqi=no`
-    )
-    //console.log(response)
-
-    const data = await response.json()
-    //console.log(data)
-
-    setCurrentLocation(data.location)
-    //console.log(currentLocation)
-
-    setCurrentConditions(data.current)
-    //console.log(currentConditions)
-
-    setCurrentConditionsSecondary(data.current.condition)
-    //console.log(currentConditionsSecondary)
-    setLoading(false)
-  }
 
   if (!loading) {
     return (
