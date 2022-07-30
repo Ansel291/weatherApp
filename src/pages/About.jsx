21cecd7ed67c2807bf7c03794d0ad489ef07741c
jason <jason@herman-scheer.com>
2022-07-30 09:54:55 -0500

    reduced font size of about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,14 @@
 function About() {
   return (
     <div className='max-w-2xl mx-auto conditions-text-color'>
-      <h1 className='text-3xl mb-[20px]'>Weather App</h1>
-      <p className='mb-[20px] text-xl font-light'>
+      <h1 className='text-[26.5px] md:text-[30px] mb-[20px] tracking-[-0.1px]'>
+        Weather App
+      </h1>
+      <p className='mb-[20px] text-[16px] md:text-[19px] font-light'>
         A React App that searches for live current weather conditions by city or
         zip code as well as displaying the 3-day forecast.
       </p>
-      <p className='text-xl font-light'>
+      <p className='text-[16px] md:text-[19px] font-light'>
         <span className='font-bold'>Version:</span> 1.0.0
       </p>
     </div>
