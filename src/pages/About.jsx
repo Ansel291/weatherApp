5903c0b519d40ff44eec4e151e7c3f237fcc88f2
jason <jason@herman-scheer.com>
2022-07-30 10:55:25 -0500

    finalized spacing changes for desktop

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,10 @@
 function About() {
   return (
     <div className='max-w-2xl mx-auto conditions-text-color'>
-      <h1 className='text-[26.5px] md:text-[30px] mb-[18px] tracking-[.25px]'>
+      <h1 className='text-[26.5px] md:text-[30px] mb-[15px] md:mb-[18px] tracking-[.25px]'>
         Weather App
       </h1>
-      <p className='mb-[20px] text-[16px] md:text-[19px] font-light'>
+      <p className='mb-[20px] text-[17px] md:text-[19px] font-light leading-[23px] md:leading-[28px]'>
         A React App that searches for live current weather conditions by city or
         zip code as well as displaying the 3-day forecast.
       </p>
